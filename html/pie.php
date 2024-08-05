c9b4af789ee7c69edb05d3624d489d06d26e0753
DieAleCamHer <dach04113@gmail.com>
2024-08-04 22:57:34 -0600

    pie de pagina (faltan cambios)

--- html/pie.php
+++ html/pie.php
@@ -9,27 +9,27 @@
                         <li>
                             <h6><b>AYUDA</b></h6>
                         </li>
-                        <li>Contáctanos</li>
-                        <li>Métodos de pago</li>
-                        <li>Cambios y devoluciones</li>
-                        <li>Preguntas frecuentes</li>
-                        <li>Promociones</li>
+                        <li><a href="../html/contactanos.php" class="text-decoration-none text-white">Contáctanos</a></li>
+                        <li><a href="../html/metodosDePago.php" class="text-decoration-none text-white">Métodos de pago</a></li>
+                        <li><a href="../html/cambios y devoluciones.php" class="text-decoration-none text-white">Cambios y devoluciones</a></li>
+                        <li><a href="../html/.php" class="text-decoration-none text-white">Preguntas frecuentes</a></li>
+                        <li><a href="../html/.php" class="text-decoration-none text-white">Promociones</a></li>
                     </ul>
                 </div>
                 <div class="col-6 col-sm-4 col-md-2">
                     <ul class="">
                         <hr>
                         <li>
-                            <h6><b>ACERCA</b></h6>
+                            <h6><b>ACERCA DE</b></h6>
                         </li>
-                        <li>Ubicación</li>
-                        <li>Redes</li>
+                        <li><a href="../html/.php" class="text-decoration-none text-white">Ubicación</a></li>
+                        <li><a href="../html/.php" class="text-decoration-none text-white">Redes</a></li>
                 </div>
                 <div class="col-6 col-sm-4 col-md-2">
                     <ul class="">
                         <hr>
                         <li>
-                            <h6><b>ACERCA</b></h6>
+                            <h6><b>ACERCA DE</b></h6>
                         </li>
                         <li>Ubicación</li>
                         <li>Redes</li>
@@ -40,9 +40,9 @@
                         <li>
                             <h6><b>NOSOTROS</b></h6>
                         </li>
-                        <li>Somos ESfly</li>
-                        <li>Alianzas y Beneficios</li>
-                        <li>Accecibilidad</li>
+                        <li><a href="../html/.php" class="text-decoration-none text-white">Somos ESfly</a></li>
+                        <li><a href="../html/.php" class="text-decoration-none text-white">Alianzas y Beneficios</a></li>
+                        <li><a href="../html/.php" class="text-decoration-none text-white">Accecibilidad</a></li>
                 </div>
                 <div class="col-6 col-sm-4 col-md-2">
                     <ul class="">
@@ -61,9 +61,9 @@
                             <h6><b>SIGUENOS</b></h6>
                         </li>
                         <i class="bi bi-whatsapp"> 7720-9970</i><br>
-                        <i class="bi bi-instagram"> ESfly_official</i><br>
-                        <i class="bi bi-facebook"> ESfly_official</i><br>
-                        <i class="bi bi-youtube"> ESfly-official</i><br>
+                        <i class="bi bi-instagram"><a href="" class="text-decoration-none text-white"> ESfly_official</a></i><br>
+                        <i class="bi bi-facebook"><a href="" class="text-decoration-none text-white"> ESfly_official</a></i><br>
+                        <i class="bi bi-youtube"><a href="" class="text-decoration-none text-white"> ESfly-official</a></i><br>
                 </div>
             </div>
         </div>
