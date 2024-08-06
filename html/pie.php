6b1bfe4369cb649bd945aacc9d8965d513608d59
DieAleCamHer <dach04113@gmail.com>
2024-08-05 22:02:36 -0600

    footer terminado :)

--- html/pie.php
+++ html/pie.php
@@ -3,67 +3,48 @@
         </div>
         <div class="container-fluid text-white">
             <div class="row">
-                <div class="col-6 col-sm-4 col-md-2">
+                <div class="col-6 col-sm-6 col-md-3">
                     <ul class="">
                         <hr>
                         <li>
                             <h6><b>AYUDA</b></h6>
                         </li>
-                        <li><a href="../html/contactanos.php" class="text-decoration-none text-white">Contáctanos</a></li>
-                        <li><a href="../html/metodosDePago.php" class="text-decoration-none text-white">Métodos de pago</a></li>
-                        <li><a href="../html/cambios y devoluciones.php" class="text-decoration-none text-white">Cambios y devoluciones</a></li>
-                        <li><a href="../html/.php" class="text-decoration-none text-white">Preguntas frecuentes</a></li>
-                        <li><a href="../html/.php" class="text-decoration-none text-white">Promociones</a></li>
+                        <li><a href="../html/contactanos.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Contáctanos</a></li>
+                        <li><a href="../html/metodosDePago.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Métodos de pago</a></li>
+                        <li><a href="../html/cambios y devoluciones.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Cambios y devoluciones</a></li>
+                        <li><a href="../html/.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Preguntas frecuentes</a></li>
+                        <li><a href="../html/.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Promociones</a></li>
                     </ul>
                 </div>
-                <div class="col-6 col-sm-4 col-md-2">
+                <div class="col-6 col-sm-6 col-md-3">
                     <ul class="">
                         <hr>
                         <li>
                             <h6><b>ACERCA DE</b></h6>
                         </li>
-                        <li><a href="../html/.php" class="text-decoration-none text-white">Ubicación</a></li>
-                        <li><a href="../html/.php" class="text-decoration-none text-white">Redes</a></li>
+                        <li><a href="../html/.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Ubicación</a></li>
+                        <li><a href="../html/.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Redes</a></li>
                 </div>
-                <div class="col-6 col-sm-4 col-md-2">
-                    <ul class="">
-                        <hr>
-                        <li>
-                            <h6><b>ACERCA DE</b></h6>
-                        </li>
-                        <li>Ubicación</li>
-                        <li>Redes</li>
-                </div>
-                <div class="col-6 col-sm-4 col-md-2">
+                <div class="col-6 col-sm-6 col-md-3">
                     <ul class="">
                         <hr>
                         <li>
                             <h6><b>NOSOTROS</b></h6>
                         </li>
-                        <li><a href="../html/.php" class="text-decoration-none text-white">Somos ESfly</a></li>
-                        <li><a href="../html/.php" class="text-decoration-none text-white">Alianzas y Beneficios</a></li>
-                        <li><a href="../html/.php" class="text-decoration-none text-white">Accecibilidad</a></li>
+                        <li><a href="../html/.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Somos ESfly</a></li>
+                        <li><a href="../html/.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Alianzas y Beneficios</a></li>
+                        <li><a href="../html/.php" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Accecibilidad</a></li>
                 </div>
-                <div class="col-6 col-sm-4 col-md-2">
-                    <ul class="">
-                        <hr>
-                        <li>
-                            <h6><b>NOSOTROS</b></h6>
-                        </li>
-                        <li>Somos ESfly</li>
-                        <li>Alianzas y Beneficios</li>
-                        <li>Accecibilidad</li>
-                </div>
-                <div class="col-6 col-sm-4 col-md-2">
+                <div class="col-6 col-sm-6 col-md-3">
                     <ul class="">
                         <hr>
                         <li>
                             <h6><b>SIGUENOS</b></h6>
                         </li>
                         <i class="bi bi-whatsapp"> 7720-9970</i><br>
-                        <i class="bi bi-instagram"><a href="" class="text-decoration-none text-white"> ESfly_official</a></i><br>
-                        <i class="bi bi-facebook"><a href="" class="text-decoration-none text-white"> ESfly_official</a></i><br>
-                        <i class="bi bi-youtube"><a href="" class="text-decoration-none text-white"> ESfly-official</a></i><br>
+                        <i class="bi bi-instagram"><a href="" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> ESfly_official</a></i><br>
+                        <i class="bi bi-facebook"><a href="" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> ESfly_official</a></i><br>
+                        <i class="bi bi-youtube"><a href="" class="link-light link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> ESfly-official</a></i><br>
                 </div>
             </div>
         </div>
